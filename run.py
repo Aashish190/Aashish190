91717e6c0fd7c4caa1a31b10fcd1401c5fb560d9
Aashish190 <164104821+Aashish190@users.noreply.github.com>
2026-04-09 08:54:30 +0545

    Refactor run.py to simplify module loading
    Removed checks for .so file existence and error handling for module loading.

--- run.py
+++ run.py
@@ -1,20 +1,6 @@
-import os, sys, importlib.util
-
-# 1. Check if .so exists
-so_file = 'filecr.so'
-if not os.path.isfile(so_file):
-    print("\033[1;31m [!] Component missing! Download again.")
-    sys.exit()
-
+import os, sys
 try:
-    # 2. Force Load Module (Internal name mismatch fix)
-    # Yesle PyInit_filecr ko error lai bypass garna khojcha
-    spec = importlib.util.spec_from_file_location("filecr", os.path.abspath(so_file))
-    module = importlib.util.module_from_spec(spec)
-    sys.modules["filecr"] = module
-    spec.loader.exec_module(module)
-    
+    import filecr
+    filecr.main() # वा filecr भित्रको मुख्य function जुन रन हुनुपर्ने हो
 except Exception as e:
-    # 3. Yadi mathi ko le kaam garena bhane, compiler mai error chha
-    print(f"\033[1;31m [!] Error: {e}")
-    print("\033[1;33m [•] Note: Tapai ko .so file 'filecr.py' name bata compile bhayeko huna parcha.")
+    print(f"Error: {e}")
