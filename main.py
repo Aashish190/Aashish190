47a4b7eaddb1fa9a594f329968df6509eaf7cc9d
Aashish190 <164104821+Aashish190@users.noreply.github.com>
2026-04-09 17:09:45 +0545

    Translate comments and error messages to Nepali
    Updated comments and error messages to Nepali language.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os, sys
 
-# .so file chha ki nai check garne
+# फाइल छ कि नाइ चेक गर्ने
 if not os.path.isfile('masu.so'):
-    print(" [!] Component missing! Download again.")
+    print(" [!] masu.so not found!")
     sys.exit()
 
 try:
-    # Timro encrypted code lai yaha bata run garchha
-    import masu
+    import masu  # यदि masu.so छ भने यसले काम गर्नुपर्छ
+    masu.__EXECUTE_CORE__() # तपाईंको स्क्रिप्टको मुख्य फङ्सन रन गर्न
 except Exception as e:
     print(f" [!] Error: {e}")
-    
